907e3161054cace5e5c9921bea73e5608d72996f
ClaudioJorge24871 <aluno24871@ipt.pt>
2025-03-22 12:19:59 +0000

    Criação e implementação de WebSockets (cliente)

---
+++ app-horarios/src/lib/websocket/client.js
@@ -0,0 +1,25 @@
+import {io} from 'socket.io-client';
+
+const createWebSocketClient = () => {
+    // Initializa o Socket.Io do lado client e config
+    // O VITE obtém o URL do WebSocket por variavel no env
+    // a variavel vai apontar para o URL do servidor
+    const socket = io(import.meta.env.VITE_WS_URL, {
+        autoConnect: false,
+        reconnection: true,
+        reconnectionAttempts: 5,
+        reconnectionDelay: 3000,
+    });
+
+    // retorno dos metodos do socket
+    return{
+        connect: () => socket.connect(),
+        disconnect: () => socket.disconnect(),
+        on: (event, callback) => socket.on(event, callback),
+        emit: (event, data) => socket.emit(event, data),
+        getSocket: () => socket,
+    };
+};
+
+const wsClient = createWebSocketClient();
+export default wsClient;
