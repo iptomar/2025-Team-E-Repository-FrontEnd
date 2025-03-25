880b13fd1799115a58e34135e696b483dc774b9a
ClaudioJorge24871 <aluno24871@ipt.pt>
2025-03-25 23:52:14 +0000

    Socket connection to App.jsx

--- app-horarios/src/App.jsx
+++ app-horarios/src/App.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './login.jsx';  
 import usePreventZoom from "./lib/utils/utils.js";
+import { useWebSocket } from './lib/websocket/hooks.jsx'; // Import the WebSocket hook
 import './App.css';
 
 function App() {
-  usePreventZoom();  
+  usePreventZoom();
+  const { isConnected } = useWebSocket(); // Get connection status from hook
 
   return (
     <Router>
       <Routes>
-        {/* Define the path for the Login page */}
         <Route path="/" element={<Login />} />
       </Routes>
     </Router>
