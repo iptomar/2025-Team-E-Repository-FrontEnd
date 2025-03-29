c9ce7da3c2373e914a320bcc48435562aaa388f6
ClaudioJorge24871 <aluno24871@ipt.pt>
2025-03-29 12:13:11 +0000

    melhoria na interface de login, agora com os temas

--- app-horarios/src/login.jsx
+++ app-horarios/src/login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import './styles/main.scss';
 
-
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -14,40 +13,51 @@ const Login = () => {
 
   return (
     <div className="login-page">
-    <div className="container-fluid d-flex">
-      <img
-        src="/iptLogin.png"
-        alt="Login"
-        className="login-image"
-      />
-      <div className="form-container">
-        <h1>Entrar na plataforma de horários</h1>
-        <form className="d-flex flex-column" onSubmit={handleSubmit}>
-          <input
-            type="email"
-            className="form-control form-control-lg mb-3"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            className="form-control form-control-lg mb-3"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {errorMessage && <div className="text-danger mb-3">{errorMessage}</div>}
-          <button
-            type="submit"
-            className="btn btn-primary btn-lg mx-auto"
-          >
-            Entrar
-          </button>
-        </form>
+      <div className="container-fluid p-0">
+        <div className="row g-0 vh-100">
+          <div className="col-md-4 d-flex align-items-center justify-content-center overflow-hidden bg-light">
+            <img
+              src="/iptLogin.png"
+              alt="Login"
+              className="login-image"
+            />
+          </div>
+
+          <div className="col-md-8 d-flex align-items-center justify-content-center bg-white">
+            <div className="form-container">
+              <h1 className="mb-5">Entrar na plataforma de horários</h1>
+              <form className="d-flex flex-column w-100" onSubmit={handleSubmit}>
+                <div className="mb-4 col-md-6">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder='Email'
+                  />
+                </div>
+                <div className="mb-5 col-md-6">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder='Senha'
+                  />
+                </div>
+                {errorMessage && <div className="text-danger mb-3">{errorMessage}</div>}
+                <button
+                  type="submit"
+                  className="btn btn-primary btn-lg"
+                >
+                  Entrar
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
