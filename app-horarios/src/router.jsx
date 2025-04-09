fab0f86ceacdf57681d79118ade51af128286240
Bruno Freitas <bmsffreitas1@gmail.com>
2025-04-09 12:23:02 +0100

    First adaptation of the old page; API not working yet; Auth not connected yet;

---
+++ app-horarios/src/router.jsx
@@ -0,0 +1,16 @@
+﻿import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import CalendarTest from "./pages/calendar-test";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/pages/calendar-test",
+        element: <CalendarTest />,
+    },
+]);
+
+export default router;
